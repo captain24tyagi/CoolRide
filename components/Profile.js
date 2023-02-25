cdac5c0124719e2c621a793daf56fe2f0b9063a4
captain24tyagi <sarthak2002tyagi@gmail.com>
2023-02-25 11:55:32 +0530

    profile

--- components/Profile.js
+++ components/Profile.js
@@ -51,7 +51,7 @@ const Profile = () => {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 1 }}
         viewport={{ once: true }}
-        className="rounded-xl border border-black border-double md:mx-20 shadow-2xl shadow-black mx-auto mt-5 md:mt-0"
+        className="rounded-xl border border-black border-double md:mx-20 shadow-2xl shadow-black mx-auto mt-5 mb-20 md:mt-0"
         src="/Phone5.png"
         alt="/"
         height={220}
